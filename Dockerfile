43a88a23b3df95452bcd83d2b2ef1b48bf0b94a8
zhaode <535900639@qq.com>
2019-10-30 14:30:12 +0800

    去除无关配置文件

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:carbon
-COPY . /sjzq-backend
-WORKDIR /sjzq-backend
-RUN npm install pm2 -g
-RUN npm install
-RUN npm run build
-EXPOSE 3000
-CMD ["pm2-runtime","start", "./start.json"]
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/naquyong/songjiang-backend:latest .
-
-docker push registry-vpc.cn-shanghai.aliyuncs.com/naquyong/songjiang-backend:latest
-
-curl 'https://cs.console.aliyun.com/hook/trigger?triggerUrl=Y2U5YzFkM2QwYjRiNzRkOWRhYTM2MzA3OTg4MmUxZDkyfHN1aW5peW9uZy1iYWNrZW5kfHJlZGVwbG95fDFhdjBicjRwcW1yNmZ8&secret=6a674a36765a4857674344586b30783787c5ac579bb52f428350536401438ba0'
